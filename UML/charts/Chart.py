a0a9e4c2f5f717456079586e72549370df233d0a
MarkTiukov <tyukov.mark@gmail.com>
2020-04-25 14:43:42 +0300

    Began implementation of lists in chart and lines

--- UML/charts/Chart.py
+++ UML/charts/Chart.py
@@ -15,7 +15,7 @@ class Chart:
 
     def __init__(self,
                  canvas: tk.Canvas,
-                 arrows: list,
+                 arrows: dict,
                  x=0, y=0,
                  width=160,
                  height=200,
@@ -23,6 +23,7 @@ class Chart:
                  thickness=4,
                  backgroundColor=Colors.LIGHT_LIGHT_GREY):
         self.canvas = canvas
+        self.arrows = arrows
         self.width = width
         self.height = height
         self.boundColor = boundColor
@@ -44,6 +45,9 @@ class Chart:
                                   (self.x - 12, self.y + self.height // 2 - 5)]
         self.deleteButton = tk.Button(text="x", width=1, height=1,
                                       bg=Colors.RED, command=self.destroy)
+        self.arrowsIn = []
+        self.arrowsOut = []
+        self.fromChart = dict()
 
     def draw(self):
         self.border = self.canvas.create_rectangle(self.x - 1, self.y - 1,
@@ -64,3 +68,22 @@ class Chart:
         self.deleteButton.destroy()
         self.canvas.delete(self.border)
         self.frame.destroy()
+        # deleting incoming arrows
+        for arrowName in self.arrowsOut:
+            print(self.arrows[arrowName].getName())
+            self.canvas.delete(self.arrows[arrowName].getObject())
+            self.arrowsOut.remove(arrowName)
+        for arrowName in self.arrowsIn:
+            self.canvas.delete(self.arrows[arrowName])
+            self.arrowsIn.remove(arrowName)
+
+    def deleteInArrow(self, name):
+        if name in self.arrowsIn:
+            self.arrowsIn.remove(name)
+
+    def addStartArrow(self, name: str):
+        self.arrowsOut.append(name)
+
+    def addEndArrow(self, name: str, fromChart):
+        self.arrowsIn.append(name)
+        self.fromChart[name] = fromChart
--- UML/main.py
+++ UML/main.py
@@ -6,6 +6,63 @@ from arrows.Line import Line
 from charts.ClassChart import ClassChart
 from charts.InterfaceChart import InterfaceChart
 
+
+def getName():
+    common = "arrow"
+    global _mynumber
+    _mynumber += 1
+    return common + str(_mynumber)
+
+
+def drawArrow(name: str):
+    arrows[name].draw()
+
+
+def createArrow(coordinates, chart):
+    global arrowStart, arrowEnd
+    global arrows, startChart
+    if arrowStart is None:
+        arrowStart = coordinates
+        startChart = chart
+    elif arrowEnd is None:
+        arrowEnd = coordinates
+        name = getName()
+        startChart.addStartArrow(name)
+        chart.addEndArrow(name, startChart)
+        arrows[name] = Line(canvas, arrowStart, arrowEnd, name)
+        drawArrow(name)
+        arrowStart = None
+        arrowEnd = None
+
+
+def popup(event):
+    global xToCreate, yToCreate
+    print("popup")
+    xToCreate = event.x
+    yToCreate = event.y
+    menu.post(event.x_root, event.y_root)
+
+
+def createChart():
+    for i in range(4):
+        charts[-1:][0].smallButtons[i]['command'] = lambda \
+                coor=charts[-1:][0].buttonCoordinates[i], \
+                chart=charts[-1:][0]: createArrow(coor, chart)
+    charts[-1:][0].draw()
+
+
+def createClass():
+    global charts
+    charts.append(ClassChart(canvas, arrows, x=xToCreate, y=yToCreate))
+    createChart()
+
+
+def createInterface():
+    global charts
+    charts.append(InterfaceChart(canvas, arrows, x=xToCreate, y=yToCreate))
+    createChart()
+
+
 mouseRightButton = \
     {"linux": "<Button-3>", "win32": "<Button-3>", "cygwin": "<Button-3>",
      "darwin": "<Button-2>"}[
@@ -22,64 +79,18 @@ canvas = tk.Canvas(root, width=windowWidth, height=windowHeight, bg='white')
 canvas.place(x=0, y=0)
 
 charts = list()
-arrows = list()
+arrows = dict()
 
 arrowStart = None
 arrowEnd = None
+startChart = None
+_mynumber = 0
 
 xToCreate = 100
 yToCreate = 100
 
 mb.showinfo("Небольшой туториал",
             "Нажми правой кнопкой там, где хочешь создать класс или интерфейс")
-
-
-def drawArrow():
-    # TODO Finish implementation charts in lines
-    arrows.append(Line(canvas, arrowStart, arrowEnd))
-    arrows[-1:][0].draw()
-
-
-def createArrow(coordinates):
-    global arrowStart, arrowEnd
-    global arrows
-    if arrowStart is None:
-        arrowStart = coordinates
-    elif arrowEnd is None:
-        arrowEnd = coordinates
-        drawArrow()
-        arrowStart = None
-        arrowEnd = None
-
-
-def popup(event):
-    global xToCreate, yToCreate
-    print("popup")
-    xToCreate = event.x
-    yToCreate = event.y
-    menu.post(event.x_root, event.y_root)
-
-
-def createChart():
-    for i in range(4):
-        charts[-1:][0].smallButtons[i]['command'] = lambda \
-                coor={"coordinates": charts[-1:][0].buttonCoordinates[i],
-                      "chart": charts[-1:]}: createArrow(coor)
-    charts[-1:][0].draw()
-
-
-def createClass():
-    global charts
-    charts.append(ClassChart(canvas, arrows, x=xToCreate, y=yToCreate))
-    createChart()
-
-
-def createInterface():
-    global charts
-    charts.append(InterfaceChart(canvas, arrows, x=xToCreate, y=yToCreate))
-    createChart()
-
-
 menu = tk.Menu(tearoff=0)
 menu.add_command(label="Class", command=createClass)
 menu.add_command(label="Interface", command=createInterface)
